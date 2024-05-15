484ae9b35a6e4f7085e1a827c8802e3d41ce05db
Jimmy Downer <jamesdanieldowner@gmail.com>
2024-05-14 21:19:06 -0600

    Refactor out GameEngine

--- src/GameEngine.hxx
+++ src/GameEngine.hxx
@@ -1,4 +1,15 @@
 #ifndef GAME_ENGINE_HXX
 #define GAME_ENGINE_HXX
 
+#include "Renderer.hxx"
+
+class GameEngine {
+public:
+  GameEngine();
+  void loop();
+
+private:
+  Renderer renderer;
+};
+
 #endif // GAME_ENGINE_HXX
